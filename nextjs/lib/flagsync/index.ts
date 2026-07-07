910c0e59ba69a10a0551b64f502db8aac5410dfe
Mike Chabot <mike.chabot@live.com>
2026-07-07 00:05:33 -0400

    Bump sdk versions

--- nextjs/lib/flagsync/index.ts
+++ nextjs/lib/flagsync/index.ts
@@ -3,6 +3,7 @@ import { getContext } from '@/lib/flagsync/user-context';
 
 export const client = createClient({
   sdkKey: process.env.FLAGSYNC_SDK_KEY!,
+  logLevel: 'DEBUG',
 });
 
 export const identify = createIdentify(getContext);
